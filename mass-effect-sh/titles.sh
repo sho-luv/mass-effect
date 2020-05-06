7e451e4925a8525f3016c4134153ebe3772f310a
sho-luv <sho.luv@gmail.com>
2020-05-06 18:41:55 -0500

    delete stuff

--- mass-effect-sh/titles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# search for titles in banners
-masscan --readscan http | grep title | grep --color=auto -i tomcat
-masscan --readscan http | grep title | grep --color=auto -i bitnami
-masscan --readscan http | grep title | grep --color=auto -i jenkins
