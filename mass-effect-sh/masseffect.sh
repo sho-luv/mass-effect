3f6832c24a7c36141552525906693e85ae15a9c1
sho-luv <sho.luv@gmail.com>
2020-07-30 07:23:37 -0700

    updated masseffect.sh to echo some commands before scan

--- mass-effect-sh/masseffect.sh
+++ mass-effect-sh/masseffect.sh
@@ -8,18 +8,43 @@ then
 fi
 
 # run masscan with known ports
-masscan --open -p U:161 -iL range.txt --excludefile exclude.txt --banners -oB snmp --rate 10000
-masscan --open -p U:500 -iL range.txt --excludefile exclude.txt --banners -oB ike --rate 10000
-masscan --open -pU:623 -iL range.txt --excludefile exclude.txt --banners -oB ipmi --rate 10000
-masscan --open -p 21 -iL range.txt --excludefile exclude.txt --banners -oB ftp --rate 10000
-masscan --open -p 22 -iL range.txt --excludefile exclude.txt --banners -oB ssh --rate 10000
-# iptables -A INPUT -p tcp --dport 60000 -j DROP
-# https://resources.infosecinstitute.com/masscan-scan-internet-minutes/
-masscan --open -p 80,443,8080,8081 -iL range.txt --excludefile exclude.txt --banners -oB http --rate 10000 --source-port 60000
-masscan --open -p 111 -iL range.txt --excludefile exclude.txt --banners -oB nfs --rate 10000
+# smb
+echo "masscan --open -p 445 -iL range.txt --excludefile exclude.txt --banners -oB smb --rate 10000"
 masscan --open -p 445 -iL range.txt --excludefile exclude.txt --banners -oB smb --rate 10000
+# http
+echo "iptables -A INPUT -p tcp --dport 60000 -j DROP"
+iptables -A INPUT -p tcp --dport 60000 -j DROP
+# https://resources.infosecinstitute.com/masscan-scan-internet-minutes/
+echo "masscan --open -p 80,443,8080,8081 -iL range.txt --excludefile exclude.txt --banners -oB http --rate 10000 --source-port 60000"
+masscan --open -p 80,443,8080,8081 -iL range.txt --excludefile exclude.txt --banners -oB http --rate 10000 --source-port 60000
+echo "iptables -D INPUT -p tcp --dport 60000 -j DROP"
+iptables -D INPUT -p tcp --dport 60000 -j DROP
+# snmp
+echo "masscan --open -p U:161 -iL range.txt --excludefile exclude.txt --banners -oB snmp --rate 10000"
+masscan --open -p U:161 -iL range.txt --excludefile exclude.txt --banners -oB snmp --rate 10000
+# ike
+echo "masscan --open -p U:500 -iL range.txt --excludefile exclude.txt --banners -oB ike --rate 10000"
+masscan --open -p U:500 -iL range.txt --excludefile exclude.txt --banners -oB ike --rate 10000
+# ipmi
+echo "masscan --open -pU:623 -iL range.txt --excludefile exclude.txt --banners -oB ipmi --rate 10000"
+masscan --open -pU:623 -iL range.txt --excludefile exclude.txt --banners -oB ipmi --rate 10000
+# ftp
+echo "masscan --open -p 21 -iL range.txt --excludefile exclude.txt --banners -oB ftp --rate 10000"
+masscan --open -p 21 -iL range.txt --excludefile exclude.txt --banners -oB ftp --rate 10000
+# ssh
+echo "masscan --open -p 22 -iL range.txt --excludefile exclude.txt --banners -oB ssh --rate 10000"
+masscan --open -p 22 -iL range.txt --excludefile exclude.txt --banners -oB ssh --rate 10000
+# nfs
+echo "masscan --open -p 111 -iL range.txt --excludefile exclude.txt --banners -oB nfs --rate 10000"
+masscan --open -p 111 -iL range.txt --excludefile exclude.txt --banners -oB nfs --rate 10000
+# rlogin
+echo "masscan --open -p 513 -iL range.txt --excludefile exclude.txt --banners -oB rlogin --rate 10000"
 masscan --open -p 513 -iL range.txt --excludefile exclude.txt --banners -oB rlogin --rate 10000
+# java-rmi
+echo "masscan --open -p 1099 -iL range.txt --excludefile exclude.txt --banners -oB java-rmi --rate 10000"
 masscan --open -p 1099 -iL range.txt --excludefile exclude.txt --banners -oB java-rmi --rate 10000
+# mysql
+echo "masscan --open -p 1433 -iL range.txt --excludefile exclude.txt --banners -oB mssql --rate 10000"
 masscan --open -p 1433 -iL range.txt --excludefile exclude.txt --banners -oB mssql --rate 10000
 masscan --open -p 1521 -iL range.txt --excludefile exclude.txt --banners -oB oracle --rate 10000
 masscan --open -p 2010,8000,9999 -iL range.txt --excludefile exclude.txt --banners -oB jdwp --rate 10000
